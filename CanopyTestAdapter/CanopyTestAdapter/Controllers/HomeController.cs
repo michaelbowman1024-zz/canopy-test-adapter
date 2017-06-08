5b3973988af35f29cb77e2603f21089fcaa18baf
Michael Bowman <mike_bowman@bobevans.com>
2017-06-08 14:14:48 -0400

    Setup basic page, view and model structure.

---
+++ CanopyTestAdapter/CanopyTestAdapter/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CanopyTestAdapter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CanopyTestAdapter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Selection()
+        {
+            return View(new List<Test>()
+            {
+                new Test()
+                {
+                    Id = 1,
+                    Title = "Test",
+                    IsSelected = false
+                }
+            });
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ CanopyTestAdapter/CanopyTestAdapter/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿namespace CanopyTestAdapter.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
